6211b01b9d0b1ea8d2a164d07693f63a1f93c9fb
TALHA BAQAR HUSSAIN <159868699+talhabaq@users.noreply.github.com>
2024-08-05 22:48:38 +0500

    Login.jsx

--- Login.jsx
+++ Login.jsx
@@ -1,12 +1,17 @@
+import { useState } from 'react'
 import { View, Text, Button, TextInput } from 'react-native'
 
 
 export const LOG = (props) => {
-    const name='BAQAR HUSSAIN'
-    const age=50
+  const [name,setName]=useState("")
+    const [age,setAge]=useState("")
     return (
       <View style={{ flex: 1, justifyContent: "center", alignItems: "center" }}>
         <Text style={{ fontSize: 30 }}>LOG IN </Text>
+        <TextInput style={{borderColor:"darkgreen", borderWidth:5,borderRadius:20, fontSize:22}} placeholder='enter name'
+        onChangeText={(name)=>{setName(name)}}/>
+          <TextInput style={{borderColor:"darkgreen", borderWidth:5,borderRadius:20, fontSize:22}} placeholder='enter age'
+        onChangeText={(age)=>{setAge(age)}}/>
         <Button title='go to home page' onPress={() => props.navigation.navigate("HOME",{name,age})} />
       </View>
     )
